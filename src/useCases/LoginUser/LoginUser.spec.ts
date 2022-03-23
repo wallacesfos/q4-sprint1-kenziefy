0ffabe642ffc2a1db281baa85b565c691267e1cc
wallacesfos <wallacesfos@hotmail.com>
2022-03-23 00:35:17 -0300

    Tests CI add

--- src/useCases/LoginUser/LoginUser.spec.ts
+++ src/useCases/LoginUser/LoginUser.spec.ts
@@ -10,6 +10,7 @@ const user = {
 
 describe("Testes user login", () => {
     it("User must be able to login", async () => {
+
         await request(app).post("/users/register").send(user)
 
         const response = await request(app).post("/users/login").send(user)
@@ -17,4 +18,31 @@ describe("Testes user login", () => {
         expect(response.statusCode).toBe(200)
     })
 
+    it("User passes invalid username", async () => {
+        const response = await request(app).post("/users/login").send({
+            username: "kenzinhoquenaoexiste",
+            password: "1231231"
+        })
+        
+        expect(response.statusCode).toBe(404)
+    })
+
+    it("User passes invalid password", async () => {
+        const response = await request(app).post("/users/login").send({
+            username: "wallace",
+            password: "123123321"
+        })
+        
+        expect(response.statusCode).toBe(401)
+    })
+
+    it("User passes keys invalid", async () => {
+        const response = await request(app).post("/users/login").send({
+            usernam: "wallace",
+            password: "123123321"
+        })
+
+        expect(response.statusCode).toBe(400)
+
+    })
 })
