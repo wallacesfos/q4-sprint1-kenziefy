aad9a6077b133772ed70eb119d8ce4b730dcdb6d
wallacesfos <wallacesfos@hotmail.com>
2022-03-24 17:46:39 -0300

    .env add

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,4 +1,9 @@
+import * as dotenv from 'dotenv';
+
+
+dotenv.config({ path: __dirname+'/.env' });
+
 export const config = {
-    secret: "the_greatest_secret_key",
+    secret: process.env.SECRET_KEY || "KENZINHO_3232323232_323232323",
     expiresIn: "1h",
 };
